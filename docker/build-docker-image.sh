7ed119b6a06bdf16ceb74d5d034bdaea175211aa
Patryk Spanily <patryks@lsnova.pl>
2020-10-16 12:15:56 +0200

    push to ATN

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -1,1 +1,5 @@
 docker build -t lsnova/logspout-logstash:1.1.0 .
+
+# push to ATN registry
+# docker tag lsnova/logspout-logstash:1.1.0 registry.atena.pl/lsn/logspout-logstash:1.1.0
+# docker push registry.atena.pl/lsn/logspout-logstash:1.1.0
